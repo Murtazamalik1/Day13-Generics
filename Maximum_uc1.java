fd4687996be3cf9543e2a0ca66a754e2b4b66e29
murtaza <murtazamalik1319@gmail.com>
2022-12-20 23:05:22 +0530

    given three int find max value

---
+++ Maximum_uc1.java
@@ -0,0 +1,21 @@
+package generics;
+
+public class Maximum_uc1 {
+
+        public static <T extends Comparable<T>> T maximum(T a, T b, T c){
+            T max = a;
+
+            if (b.compareTo(max) > 0)
+                max = b;
+            if (c.compareTo(max) > 0)
+                max = c;
+
+            return max;
+        }
+
+        public static void main(String[] args) {
+            System.out.println("the maximum number is :" +maximum(3, 4, 8));
+
+        }
+    }
+
