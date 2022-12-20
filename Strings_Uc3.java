2369ea9a3ec07cf619ca2e36257cac6be432e888
murtaza <murtazamalik1319@gmail.com>
2022-12-20 23:11:00 +0530

    given three strings and find max

---
+++ Strings_Uc3.java
@@ -0,0 +1,22 @@
+package generics;
+
+public class Strings_Uc3 {
+
+        public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
+            T max = a;
+
+            if (b.compareTo(max) > 0)
+                max = b;
+            if (c.compareTo(max) > 0)
+                max = c;
+
+            return max;
+        }
+
+
+    public static void main(String[] args) {
+        System.out.println("the maximum String is :" + maximum("generic", "method", "oops"));
+
+
+    }
+}
